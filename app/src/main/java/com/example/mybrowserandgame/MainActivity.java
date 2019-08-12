c36a3ba164e72e6ce08296b4fc0f2f6cac8ce94f
zamay <mityazamay@gmail.com>
2019-08-12 16:39:31 +0300

    - добавил PACKAGE_NAME - вынес код из ответа в другую функцию parseData

--- app/src/main/java/com/example/mybrowserandgame/MainActivity.java
+++ app/src/main/java/com/example/mybrowserandgame/MainActivity.java
@@ -15,6 +15,10 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.net.HttpURLConnection;
 import java.util.Random;
 
@@ -26,16 +30,22 @@ public class MainActivity extends AppCompatActivity {
     private Runnable statusUpdateRunnable;
     private ProgressBar spinner;
 
+    public static String promoType, promoUrl;
+    public static String PACKAGE_NAME;
+
     RequestQueue queue;
-    String url ="https://a.lucky-games.online/click?" +
-            "pid=720&" +
-            "offer_id=3048&" +
-            "l=1554545944";
+
+    //    String url = "http://android-app-rest.zinenko.net/api/entry";
+    String url = "http://android-app-rest.zinenko.net/api/entry?package=" + PACKAGE_NAME;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        spinner = (ProgressBar)findViewById(R.id.progressBar1);
+        spinner.setVisibility(View.VISIBLE);
 
         queue = Volley.newRequestQueue(this);
 
@@ -46,10 +56,6 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().getUserAgentString();
         webView.setWebViewClient(new CustomWebViewClient());
 
-        spinner = findViewById(R.id.progressBar1);
-        spinner.setVisibility(View.VISIBLE);
-
-//        webView.loadUrl("file:///android_asset/cube-loader.html");
         volleyServer();
     }
 
@@ -58,36 +64,52 @@ public class MainActivity extends AppCompatActivity {
                 new Response.Listener<String>() {
                     @Override
                     public void onResponse(String response) {
-//                       Log.i("response", response);
-
-                       // Проверка что показывать, а пока
-                        Random r = new Random();
-                        int choice = r.nextInt(2);
-                        if(choice==0) {
-                            url = "file:///android_asset/game-js.html";
-                        }
-                        spinner.setVisibility(View.GONE);
-                        webView.loadUrl(url);
+                        Log.d("Successful response", "Successful response");
+                        parseData(response);
                     }
                 }, new Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
                         Log.e("onErrorResponse", String.valueOf(error));
                     }
-                }){
-                    @Override
-                    public void deliverError(final VolleyError error) {
-                        Log.i("deliverError", error.networkResponse.headers.get("Location"));
-                        final int status = error.networkResponse.statusCode;
-                        if(HttpURLConnection.HTTP_MOVED_PERM == status ||
-                                status == HttpURLConnection.HTTP_MOVED_TEMP ||
-                                status == HttpURLConnection.HTTP_SEE_OTHER) {
-                            url = error.networkResponse.headers.get("Location");
-                            volleyServer();
-                        }
-                    }
-                };
+                });
+//                {
+//                    @Override
+//                    public void deliverError(final VolleyError error) {
+//                        Log.i("deliverError", error.networkResponse.headers.get("Location"));
+//                        final int status = error.networkResponse.statusCode;
+//                        if(HttpURLConnection.HTTP_MOVED_PERM == status ||
+//                                status == HttpURLConnection.HTTP_MOVED_TEMP ||
+//                                status == HttpURLConnection.HTTP_SEE_OTHER) {
+//                            url = error.networkResponse.headers.get("Location");
+//                            volleyServer();
+//                        }
+//                    }
+//                };
         queue.add(stringRequest);
+    }
+
+    public void parseData(String response) {
+        try {
+            JSONObject jsonObject = new JSONObject(response);
+            promoType = jsonObject.getString("promoType");
+            promoUrl = jsonObject.getString("promoUrl");
+
+            if(promoType.equals("l") ) {
+                url = promoUrl;
+            } else if (promoType.equals("p")){
+                url = promoUrl;
+            } else if (promoType.equals("w")) {
+                url = "file:///android_asset/game-js.html";
+            }
+
+            Log.i("url", url);
+            spinner.setVisibility(View.GONE);
+            webView.loadUrl(promoUrl);
+        } catch (JSONException e) {
+            Log.e("parseDataError", e.getMessage());
+            e.printStackTrace();
+        }
     }
 
     // Кнопка назад
